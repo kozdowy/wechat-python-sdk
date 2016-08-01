767afaeba4b54bdc2cba2b6fa455b1e47993deed
kozdowy <kozdowy@umich.edu>
2016-08-01 13:53:15 +0800

    apparently didn't need to change it?

--- wechat_sdk/lib/request.py
+++ wechat_sdk/lib/request.py
@@ -87,7 +87,6 @@ class WechatRequest(object):
 
         split_kwargs = []
         if isinstance(kwargs.get("data_list", ""), list):
-            new_kw = {}
             for data in kwargs.get("data_list"):
                 new_kw = copy.deepcopy(kwargs)
                 body = data
