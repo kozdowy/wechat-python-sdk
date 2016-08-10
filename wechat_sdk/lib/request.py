ed93ad01004de4faad39a4d90dce92ea6125f54f
kozdowy <kozdowy@umich.edu>
2016-08-10 17:03:55 +0800

    mass get actually works now

--- wechat_sdk/lib/request.py
+++ wechat_sdk/lib/request.py
@@ -102,6 +102,8 @@ class WechatRequest(object):
                 new_kw = copy.deepcopy(kwargs)
                 param["access_token"] = access_token
                 new_kw["params"] = param
+                del new_kw["param_list"]
+                print(new_kw)
                 split_kwargs.append(new_kw)
 
         rs = (grequests.request(method=method,
